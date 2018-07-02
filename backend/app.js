7d16f72810bdf54ed1d9cec0eb645d884aa4a933
Damian Giedrojć <icelookpl@gmail.com>
2018-07-02 22:23:02 +0200

    Final Init

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.get("/api/test", (req, res, next) => {
+  res.status(200).send("działa");
+});
+
+module.exports = app;
