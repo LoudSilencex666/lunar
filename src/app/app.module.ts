fab8f128603b07c06c2011e5c6fb59147fb67348
M4Ch000 <34491225+M4Ch000@users.noreply.github.com>
2018-07-10 00:56:00 +0200

    /auth basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'auth',
+        component: AuthComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
