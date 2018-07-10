c37ea72b35eceac5eac31a32371cdbaa1df92b07
Damian Giedrojć <giedrojcfrontend@gmail.com>
2018-07-10 11:39:24 +0200

    working with .env files

--- backend/dbconnect.js
+++ backend/dbconnect.js
@@ -7,8 +7,9 @@ const connection = mysql.createConnection({
     host     : '83.15.215.203',
     port     : '3306',
     user     : 'root',
-    password : 'pHT2yU6B_-NB',
+    password : 'pHT2yU6B_-NB', // process.env.PASSWORD_FOR_DB
     database : 'test'
-  });
+});
 
+console.log(process.env.PASSWORD_FOR_DB)
 module.exports = connection;
