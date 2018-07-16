c4c7270024ac4c4006da5b60ee692d62c59d4053
M4Ch000 <34491225+M4Ch000@users.noreply.github.com>
2018-07-16 20:58:20 +0200

    db stuff

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
 const dbPool = require('../dbconnect');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    dbPool.query('SELECT * FROM zipa')
-        .then( result => { console.log(result); })
-        .then( () => { res.status(200).send("chuj w to"); });
-
-
-
+    dbPool.query('SELECT * FROM zipa').then( result =>{
+        console.log(result);
+    }).then( () => {
+        res.status(200).send("chuj w to");
+    });
 });
 
 module.exports = router;
