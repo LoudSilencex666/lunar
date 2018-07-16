d9a9ae69e269eb4e2bae52491c93934fcd752ca6
M4Ch000 <34491225+M4Ch000@users.noreply.github.com>
2018-07-16 14:48:01 +0200

    animated sidebar + global css cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -22,7 +23,7 @@ import { UsersModule } from './users/users.module';
     imports: [
       AppRoutingModule,
       BrowserModule,
-
+      BrowserAnimationsModule,
       AuthModule,
       HomeModule,
       SharedModule,
