f50120430a1b29e863af097a06eb3abebf500a81
M4Ch000 <34491225+M4Ch000@users.noreply.github.com>
2018-07-16 20:15:42 +0200

    home components

---
+++ src/app/home/chart/chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-home-chart',
+    templateUrl: './chart.component.html',
+    styleUrls: ['./chart.component.css']
+})
+
+export class ChartComponent {}
