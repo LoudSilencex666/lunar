6a93fa2eda31a59b2ce9bb2779b5c10b9483dd49
Damian Giedrojć <giedrojcfrontend@gmail.com>
2018-07-22 17:18:09 +0200

    default layout structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -19,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
         ReactiveFormsModule,
         SidebarComponent,
         HeaderComponent,
+        FooterComponent,
         HttpClientModule
         // FormsModule
     ]
