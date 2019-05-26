025834c63aec4f5bbb43c143339f6ae7fbaf6242
Damian Giedrojć <icelookpl@gmail.com>
2019-05-26 18:59:53 +0200

    nie wiem nawet po co to

--- src/app/core/models/index.ts
+++ src/app/core/models/index.ts
@@ -1,7 +1,3 @@
 export * from './auth.model';
 export * from './news.model';
-<<<<<<< HEAD
-export * from './news_content.model';
 export * from './user.model';
-=======
->>>>>>> b5a1de449d57828785ed2ddb332c91a852caffbb
